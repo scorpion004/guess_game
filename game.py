8e1a17d2d3d23258dbdfee2f7647f4ff3728f57b
karborus <31381174+scorpion004@users.noreply.github.com>
2019-06-02 11:55:06 +0530

    first commit
    This is a stone-paper-scissor guess game based on python. If you want run this code, you have to install python 3. Follow the steps to run this game.
    
    How to run this game
    -------------------------------------
    1. Download or clone the folder
    2. open the folder
    3. open terminal or cmd in this folder or you can copy and paste the path
    4. type python game.py
    5. Enjoy the game
    
    In case you want to modify it, you are always welcome.
    If you want to contact with me, you can mail me.   scorpion004ss@gmail.com

---
+++ game.py
@@ -0,0 +1,57 @@
+import random
+"""
+welcome to my stone-paperf-scissor game made of python language
+Programmer: Shuvasis
+language: Python
+Email: scorpion004ss@gmail.com
+"""
+total_input_left=10
+no_of_computer_win=0
+no_of_human_win=0
+
+
+print("\t \t \t  s  =  stone  :   p  =  paper  :  k =  scissor (....Enter q to stop)  \n")
+
+while (total_input_left>0):
+    print(f"\n[ Your chance left {total_input_left} ]")
+    total_input_left-=1
+    human_input = input("Enter Your Choice.......  \n")
+    human_input=human_input.lower()
+    computer_input = (random.choice(["s","p","k"]))
+
+    if human_input=="q":
+        print(".........GAME END.........")
+        break
+
+    elif human_input == computer_input:
+        print("Both are same !!!!!")
+
+    elif human_input == 's' and computer_input == 'p':
+        print("Computer won.....You lose....!!")
+        no_of_computer_win+=1
+
+    elif human_input == 'p' and computer_input == 's':
+        print("You Won !!")
+        no_of_human_win+=1
+
+    elif human_input == 's' and computer_input == 'k':
+        print("You Won !!")
+        no_of_human_win+=1
+
+    elif human_input == 'k' and computer_input == 's':
+        print("Computer won.....You lose....!!")
+        no_of_computer_win+=1
+
+    elif human_input == 'k' and computer_input == 'p':
+        print("You Won !!")
+        no_of_human_win+=1
+
+    elif human_input == 'p' and computer_input == 'k':
+        print("Computer won.....You lose....!!")
+        no_of_computer_win+=1
+
+    else:
+        print("You Entered Wrong input....try again")
+
+
+print(f"\nComputer score {no_of_computer_win}\n Your score {no_of_human_win}")
